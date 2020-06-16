61deb604729c9bce95210a1f11dca4a6e7ce3047
MenFester <13500177552@qq.com>
2020-06-16 17:46:49 +0800

    2020年06月16日 17:46

---
+++ Chapter3/mainModel.js
@@ -0,0 +1,8 @@
+let { BrowserWindow } = require('electron');
+
+exports.makeWin = function() {
+    let win = new BrowserWindow({
+        webPreferences: { nodeIntegration: true }
+    });
+    return win;
+}
