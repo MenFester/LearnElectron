2e8ad7d7ed3ce780744de53a13effcdf8cd0ac71
MenFester <13500177552@qq.com>
2020-06-22 18:26:28 +0800

    2020年06月22日 18:26

--- Chapter3/index.js
+++ Chapter3/index.js
@@ -7,12 +7,13 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      enableRemoteModule: true    // 消除使用remote模块时的后台告警
     }
-  })
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -21,16 +22,16 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
@@ -38,7 +39,16 @@ app.on('activate', function () {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+const { ipcMain } = require('electron');
+ipcMain.on('msg_render2main', (event, param1, param2) => {
+  console.log(param1);
+  console.log(param2);
+  console.log(event.sender);
+
+  event.sender.send('msg_main2render', param1, param2);
+});
--- Chapter3/mainModel.js
+++ Chapter3/mainModel.js
@@ -2,7 +2,7 @@ let { BrowserWindow } = require('electron');
 
 exports.makeWin = function() {
     let win = new BrowserWindow({
-        webPreferences: { nodeIntegration: true }
+        webPreferences: { nodeIntegration: true, enableRemoteModule: true }
     });
     return win;
 }
